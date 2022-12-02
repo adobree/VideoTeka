3c8cfb5bd24b912e8ab2353607b0788682ce364a
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-12-02 13:26:23 +0100

    RAKTAR user added

--- Form1.cs
+++ Form1.cs
@@ -5,12 +5,12 @@ namespace VT
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() 
         {
             InitializeComponent();
             //Letrehoz();
             //Lement();
-            Betolt();
+            Betolt();  
         }
 
         private void Lement()
@@ -20,17 +20,18 @@ namespace VT
 
         private void Letrehoz()
         {
-      dsVT.dtFilmek.AdddtFilmekRow(1, "Godzilla", 1990);
-      dsVT.dtFilmek.AdddtFilmekRow(2, "Micsoda No", 1992);
-      dsVT.dtFilmek.AdddtFilmekRow(3, "Haz a tonal", 1993);
-      dsVT.dtFilmek.AdddtFilmekRow(4, "Erin Brockovich - Zûrös természet", 1990);
-      dsVT.dtFilmek.AdddtFilmekRow(5, "Batman", 1995);
-      dsVT.dtUser.AdddtUserRow(1, "Kovacs Adam", "ado.kovacs@gmail.com");
-      dsVT.dtUser.AdddtUserRow(2, "Nagy Gabor", "anagybogar@gmail.com");
-      dsVT.dtUser.AdddtUserRow(3, "Komives-Kaldi Csilla", "komivescs@gmail.com");
-      dsVT.dtFU.AdddtFURow(1, 1);
-      dsVT.dtFU.AdddtFURow(2, 3);
-    }
+            dsVT.dtFilmek.AdddtFilmekRow(1, "Godzilla", 1990);
+            dsVT.dtFilmek.AdddtFilmekRow(2, "Micsoda No", 1992);
+            dsVT.dtFilmek.AdddtFilmekRow(3, "Haz a tonal", 1993);
+            dsVT.dtFilmek.AdddtFilmekRow(4, "Erin Brockovich - Zûrös természet", 1990);
+            dsVT.dtFilmek.AdddtFilmekRow(5, "Batman", 1995);
+            dsVT.dtUser.AdddtUserRow(1, "Kovacs Adam", "ado.kovacs@gmail.com");
+            dsVT.dtUser.AdddtUserRow(2, "Nagy Gabor", "anagybogar@gmail.com");
+            dsVT.dtUser.AdddtUserRow(3, "Komives-Kaldi Csilla", "komivescs@gmail.com");
+            dsVT.dtUser.AdddtUserRow(4, "RAKTAR", "raktar@raktar.com");
+            dsVT.dtFU.AdddtFURow(1, 1);
+            dsVT.dtFU.AdddtFURow(2, 3);
+        }
 
         private void Betolt()
         {
@@ -56,5 +57,12 @@ namespace VT
       dgv.Dock = DockStyle.Fill;
       dgv.DataSource = dsVT.dtUser;
     }
+
+        private void raktárToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dgv.Visible = true;
+            dgv.Dock = DockStyle.Fill;
+            dgv.DataSource = dsVT.dtUser;
+        }
     }
 }
